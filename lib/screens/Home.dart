0c9c513121285d24f2bb983c4b591a5bbd2784f0
Yazeed Gazalat <105671860+YazeedGhazalat@users.noreply.github.com>
2022-10-22 19:22:10 +0300

    this is last update

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -2,9 +2,11 @@ import 'package:citycafe_app/screens/admin/adminUserScreen.dart';
 import 'package:citycafe_app/screens/normalUserScreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
 
 late User signInUser;
+String? AdminRole;
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -41,52 +43,14 @@ class _HomeState extends State<Home> {
       'm.almsedin@gmail.com',
       "a@gmail.com"
     ];
+    List roleList = [
+      'Admin',
+      "Super Admin",
+    ];
+    print("**********$AdminRole");
     if (adminList.contains(signInUser.email))
       return adminPage();
     else
       return AnyUserPage();
   }
 }
-
-// adminList() {
-//   List adminList = [];
-// }
-
-
-//  List adminList = [
-//       'Admin',
-//       "Super Admin",
-//     ];
-//     String? adminRole;
-//     String? userID;
-//     return StreamBuilder(
-//       stream: FirebaseFirestore.instance.collection("users").snapshots(),
-//       builder: (BuildContext context, AsyncSnapshot snapshot) {
-//         if (snapshot.hasError) {
-//           return Text("there is an error");
-//         }
-
-//         if (snapshot.hasData) {
-//           final admins = snapshot.data!.docs;
-//           for (var admin in admins) {
-//             final role = admin['role'];
-//             final Email = admin['UserEmail'];
-//             adminRole = role;
-//             userID = Email;
-//           }
-//           if (signInUser.email == userID &&
-//               adminList.contains(adminRole.toString())) {
-//             print('******${adminRole}');
-//             return adminPage();
-//           } else if (!adminList.contains(adminRole.toString())) {
-//             print(adminRole);
-//             return AnyUserPage();
-//           }
-//         }
-//         return const CircularProgressIndicator.adaptive();
-//       },
-//     );
-//   }
-// }
-
-// class adminList
